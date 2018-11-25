c513673c8b42835efa79b04ca3feb535b11b9f07
Patrick A Kim <pkim050@gmail.com>
2018-11-25 19:31:37 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
 document.body.appendChild(element);
+element.style.textAlign = 'center';
+var ul = document.createElement('ul');
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+element.appendChild(ul);
